4b020fbf60b9bbe37e52b353271bb88ae1faf02e
Brit Butler <redline6561@gmail.com>
2016-06-28 12:11:15 -0400

    Add functions to pull down repos and update email.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,46 @@
+import $ from 'jquery';
 import {gh_token} from "./credentials";
+
+var username = "tonyjsouthern";
+var baseURL = "https://api.github.com";
+
+$.ajaxSetup({
+  headers: {
+    Authorization: `token ${gh_token}`
+  }
+});
+
+function getRepos (username) {
+  $.ajax({
+    url: `${baseURL}/users/${username}/repos`,
+    // method: "GET",
+    dataType: 'json',
+    data: {
+      type: "owner",
+      sort: "updated"
+    }
+  }).then(function (data) {
+    console.log("the repos are", data);
+  });
+};
+
+function changeEmail (newEmail) {
+  $.ajax({
+    url: `${baseURL}/user`,
+    method: "PATCH",
+    data: JSON.stringify({
+      email: newEmail
+    })
+  }).then(function (data) {
+    console.log("updated user", data);
+  });
+};
+
+changeEmail("brit@kingcons.io");
+
+// var requestWithParams = $.ajax({
+//   url: baseURL + 'user/repos',
+//   data: {
+//     access_token: gh_token
+//   }
+// });
